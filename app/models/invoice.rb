1869b0da4b68c5f74f32b0f65d997ca5734aa7f7
Armando Andini <armando.andini@hotmail.com>
2016-09-24 12:09:39 -0300

    Calculations for taxes and totals, test and fixtures.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,17 @@ class Invoice < ActiveRecord::Base
   monetize :taxed_105_cents
   monetize :not_taxed_cents
   monetize :exempt_cents
+
+  def tax_21_cents
+    (taxed_21_cents * 0.21).to_i
+  end
+
+  def tax_105_cents
+    (taxed_105_cents * 0.105).to_i
+  end
+
+  def total_cents
+    taxed_21_cents + taxed_105_cents + tax_21_cents + tax_105_cents +
+      exempt_cents + not_taxed_cents
+  end
 end
--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class InvoiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @invoice = invoices :venta
+  end
+
+  test "tax_21 calculation" do
+    assert_equal 210, @invoice.tax_21_cents
+  end
+
+  test "tax_105 calculation" do
+    assert_equal 105, @invoice.tax_105_cents
+  end
+
+  test "total calculation" do
+    assert_equal 4315, @invoice.total_cents
+  end
 end
